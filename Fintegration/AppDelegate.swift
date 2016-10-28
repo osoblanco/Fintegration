a76b3f2dd45f444342faaa74d1492d8e06bd35a6
Hrach <hrach@Hrachs-Mac.local>
2016-10-28 12:58:32 -0700

    Fixed shit

--- Fintegration/AppDelegate.swift
+++ Fintegration/AppDelegate.swift
@@ -1,13 +1,12 @@
 //
 //  AppDelegate.swift
-//  SwiftStockExample
+//  fintegration
 //
-//  Created by Mike Ackley on 5/3/15.
-//  Copyright (c) 2015 Michael Ackley. All rights reserved.
+//  Created by Hrach on 10/28/16.
+//  Copyright © 2016 fin. All rights reserved.
 //
 
 import UIKit
-import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,20 +14,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    fileprivate func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [AnyHashable: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         UINavigationBar.appearance().barTintColor = UIColor(red: (127/255.0), green: (50/255.0), blue: (198/255.0), alpha: 1)
         let titleTextAttrs = [ NSForegroundColorAttributeName: UIColor.white ]
-        UINavigationBar.appearance().tintColor = UIColor.white 
+        UINavigationBar.appearance().tintColor = UIColor.white
         UINavigationBar.appearance().titleTextAttributes = titleTextAttrs
         UINavigationBar.appearance().isTranslucent = false
+        // Override point for customization after application launch.
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
@@ -37,7 +36,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
