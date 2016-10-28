1694db44bc2cd4c55dedbe73cf440335f05ff832
Erik Arakelyan <erika@wolfram.com>
2016-10-29 02:13:21 +0400

    added url opening integration

--- Fintegration/BotViewController.swift
+++ Fintegration/BotViewController.swift
@@ -11,31 +11,43 @@ import RxSwift
 import RxCocoa
 import Moya
 
-class BotViewController: UIViewController {
+class BotViewController: UIViewController ,UITextViewDelegate {
 
+    @IBOutlet weak var botResponseTextView: UITextView!
+    
     @IBOutlet weak var searchTextField: UITextField!
     @IBOutlet weak var askButton: UIButton!
     @IBOutlet weak var responseLabel: UILabel!
+    
     override func viewDidLoad() {
+        
+    
         super.viewDidLoad()
+        
+
 
         // Do any additional setup after loading the view.
     }
 
+    override func viewDidAppear(_ animated:Bool)
+    {
+        super.viewDidAppear(animated)
+        botResponseTextView.isSelectable = true
+        botResponseTextView.isEditable = false
+        botResponseTextView.text = "www.google.com"
+    }
+    
+    func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange) -> Bool {
+        
+
+        UIApplication.shared.openURL(URL as URL)
+        return false
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
